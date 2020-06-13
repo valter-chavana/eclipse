eb65f22ee8244932e7451b49153845fb19f1fa01
Valter Pedro Jr Chavana <66696674+valter-chavana@users.noreply.github.com>
2020-06-13 14:23:42 -0700

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/chavana/git/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.chavana.git;
+
+/**
+ * @author Valter Pedro
+ * Exemplo de integracao do git e o eclipse
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+
+	}
+
+}
